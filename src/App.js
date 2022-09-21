baf3e6db6d2cc6d34fe1a73e6451fb13ef82c59b
Ariel Turchinsky <ariel7590@gmail.com>
2022-09-21 20:36:43 +0300

    added cartIcon and cartDropdown components to the app, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import {Route, Switch, Redirect} from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch, Redirect } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
-const HatsPage=()=>(
-  <div>
-    <h1>Hats Page!</h1>
-  </div>
-)
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page!</h1>
+	</div>
+);
 
 class App extends React.Component {
+	unsubscribeFromAuth = null;
 
-  unsubscribeFromAuth=null;
+	componentDidMount() {
+		const { setCurrentUser } = this.props;
 
-  componentDidMount(){
-    const {setCurrentUser} = this.props;
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
-        const userRef=await createUserProfileDocument(userAuth);
+				userRef.onSnapshot((snapshot) => {
+					setCurrentUser({
+						id: snapshot.id,
+						...snapshot.data(),
+					});
+				});
+			} else {
+				setCurrentUser(userAuth);
+			}
+		});
+	}
 
-        userRef.onSnapshot(snapshot => {
-          setCurrentUser({
-            id: snapshot.id,
-              ...snapshot.data()
-          });
-        })
-      }
-      else{
-        setCurrentUser(userAuth);
-      }
-    })
-  }
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();
-  }
-
-  render(){return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
-      </Switch>
-    </div>
-  );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route
+						exact
+						path='/signin'
+						render={() =>
+							this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp />
+						}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps =({user}) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser,
+});
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
