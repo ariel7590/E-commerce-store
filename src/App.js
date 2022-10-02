816f86aaddffcf86ce47f94fa5232caf0ce38fc3
Ariel Turchinsky <ariel7590@gmail.com>
2022-10-02 23:13:52 +0300

    implemented code that pulls in the shop data from firestore and stores it into the shop reducer from the shop page, also included the withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import {
+	auth,
+	createUserProfileDocument,
+} from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
@@ -65,8 +68,8 @@ class App extends React.Component {
 	}
 }
 
-const mapStateToProps = createStructuredSelector ({
-	currentUser: selectCurrentUser
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,49 +1,83 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
-    apiKey: "AIzaSyB1e_8ub8iC-6qaOYdQkDTaOFGnWlmQ_7c",
-    authDomain: "crwn-db-9514f.firebaseapp.com",
-    projectId: "crwn-db-9514f",
-    storageBucket: "crwn-db-9514f.appspot.com",
-    messagingSenderId: "867352656870",
-    appId: "1:867352656870:web:437ff61f1d11bf2809b427",
-    measurementId: "G-KCMPEHRTHR"
-  };
+	apiKey: "AIzaSyB1e_8ub8iC-6qaOYdQkDTaOFGnWlmQ_7c",
+	authDomain: "crwn-db-9514f.firebaseapp.com",
+	projectId: "crwn-db-9514f",
+	storageBucket: "crwn-db-9514f.appspot.com",
+	messagingSenderId: "867352656870",
+	appId: "1:867352656870:web:437ff61f1d11bf2809b427",
+	measurementId: "G-KCMPEHRTHR",
+};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+	if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+	const snapShot = await userRef.get();
 
-  if(!snapShot.exists){
-    const{displayName, email} = userAuth;
-    const createdAt=new Date();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-    try{
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      })
-    }catch(error){
-      console.log('error creating user', error.message);
-    }
-  }
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error creating user", error.message);
+		}
+	}
 
-  return userRef;
-}
+	return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	const collectionRef = firestore.collection(collectionKey);
+
+	const batch = firestore.batch();
+	objectsToAdd.forEach((obj) => {
+		const newDocRef = collectionRef.doc();
+		console.log(newDocRef);
+		batch.set(newDocRef, obj);
+	});
+
+	return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+	const transfomedCollections = collections.docs.map((doc) => {
+		const { title, items } = doc.data();
+
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items,
+		};
+	});
+
+	return transfomedCollections.reduce((accumulator,collection)=>{
+		accumulator[collection.title.toLowerCase()]=collection;
+		return accumulator;
+	}, {})
+};
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
