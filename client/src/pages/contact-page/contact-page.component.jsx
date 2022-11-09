414f7e9933561fb008bdfa61721eae7f6524ad16
Ariel Turchinsky <ariel7590@gmail.com>
2022-11-09 16:23:16 +0200

    used nodemailer to have the ability to send emails from the server, created a beutiful email template, connected between the contact page and nodemailer using axios

--- client/src/pages/contact-page/contact-page.component.jsx
+++ client/src/pages/contact-page/contact-page.component.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import FormInput from "../../components/form-input/form-input.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import { sendMessageStart } from "../../redux/contact/contact.actions";
+import axios from "axios";
 import "./contact-page.styles.scss";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 
 const ContactPage = () => {
 	const [message, setMessage] = useState({
@@ -13,30 +15,62 @@ const ContactPage = () => {
 
 	const { subject, content } = message;
 
-	const dispatch= useDispatch();
+	const dispatch = useDispatch();
+	const currentUser = useSelector(selectCurrentUser);
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setMessage({ ...message, [name]: value });
 	};
+
+	const handleClick = (e) => {
+		e.preventDefault();
+
+		if (currentUser !== null) {
+			const { displayName, email } = currentUser;
+			axios({
+				url: "email",
+				method: "post",
+				data: {
+					subject,
+					content,
+					displayName,
+					email,
+				},
+			})
+				.then((response) => {
+					alert("Email sent successfuly");
+					setMessage({ subject: "", content: "" });
+				})
+				.catch((error) => {
+					console.log("Error ", error.message);
+					alert("There was an issue with sending the email!");
+				});
+		} else {
+			alert("You have to sign-in in order to contact us!");
+		}
+	};
+
 	return (
 		<div className='contact-page'>
-			<FormInput
-				name='subject'
-				type='text'
-				label='Subject'
-				value={subject}
-				handleChange={handleChange}
-				required
-			/>
-			<textarea
-				className='content'
-				placeholder='Message'
-				name='content'
-				value={content}
-				onChange={handleChange}
-			/>
-			<CustomButton className='send-button' onClick={()=>dispatch(sendMessageStart(message))}>Send</CustomButton>
+			<form onSubmit={handleClick}>
+				<FormInput
+					name='subject'
+					type='text'
+					label='Subject'
+					value={subject}
+					handleChange={handleChange}
+					required
+				/>
+				<textarea
+					className='content'
+					placeholder='Message'
+					name='content'
+					value={content}
+					onChange={handleChange}
+				/>
+				<CustomButton className='send-button'>Send</CustomButton>
+			</form>
 		</div>
 	);
 };
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
-const enforce = require("express-sslify");
+// const enforce = require("express-sslify");
+const nodemailer = require("nodemailer");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -14,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
@@ -45,6 +46,51 @@ app.post("/payment", (req, res) => {
 			res.status(500).send({ error: stripeErr });
 		} else {
 			res.status(200).send({ success: stripeRes });
+		}
+	});
+});
+
+function replaceAll(originalStr, find, replace) {
+	var str = originalStr;
+	return str.replace(new RegExp(find, "g"), replace);
+}
+
+app.post("/email", (req, res) => {
+	var transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: "maillaproyectim",
+			pass: "bjicyyxrkoneodyb",
+		},
+	});
+
+	const { subject, content, displayName, email } = req.body;
+	const mailBody = {
+		from: email,
+		to: "ariel7590@gmail.com",
+		subject: "Crwn-clothing contact: " + subject,
+		html: `
+			<div dir="ltr" style="border:3px solid black; margin: auto; width: 388px; height: 500px; border-radius: 8px">
+				<div style="padding: 10px 2px; display:flex; justify-content: space-between;">
+					<img src="https://iili.io/y2f7CG.png" alt="crwn-clothing" width="70" height="70">
+					<h1 style="font-family: 'open sans serif'; margin-left:20px; color: #abacaf;">Crwn-clothing-email</h1>
+				</div>
+				<br>
+				<div style="margin-top: 30px; padding: 10px">
+					From: ${displayName}
+					<br>
+					email: ${email}
+					<br><br>
+					${replaceAll(content, "\n", "<br>")}
+				</div>
+			</div>`,
+	};
+
+	transporter.sendMail(mailBody, (error, info) => {
+		if (error) {
+			res.status(500).send({ error: error });
+		} else {
+			res.status(200).send({ success: info });
 		}
 	});
 });
